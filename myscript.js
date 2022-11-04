51eb28761553c570f8a4eaad4db5db1547058314
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-11-04 11:50:10 +0100

    test

---
+++ myscript.js
@@ -0,0 +1,10 @@
+function testResults (form) {
+    var TestVar = form.inputbox.value;
+    alert ("You typed: " + TestVar);
+}
+
+function testResults2 (form) {
+    var TestVar = form.inputbox.value;
+    TestVar = TestVar + " reee";
+    alert ("You goofed: " + TestVar);
+}
