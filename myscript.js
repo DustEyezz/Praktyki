b1b1a97355d5f77a76929a8581bc24008a2ac06d
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-11-08 12:56:56 +0100

    commit

--- myscript.js
+++ myscript.js
@@ -1,10 +1,61 @@
-function testResults (form) {
-    var TestVar = form.inputbox.value;
-    alert ("You typed: " + TestVar);
+const people1 = ["John Doe", "Jane Lopez"];
+const people2 = ["John Smith", "Kyle Rogers"];
+const numbers = [1, 2, 5, 10, 20];
+
+function getLast(array){
+    return array[array.length-1];
 }
 
-function testResults2 (form) {
-    var TestVar = form.inputbox.value;
-    TestVar = TestVar + " reee";
-    alert ("You goofed: " + TestVar);
+function joinArrays(array1, array2){
+    let resArray = new Array;
+    for (i = 0; i < array1.length; i++){
+        resArray.push(array1[i]);
+    }
+    for (i = 0; i < array2.length; i++){
+        resArray.push(array2[i]);
+    }
+    return resArray;
 }
+
+function filterArray(array, filter){
+    let resArray = new Array;
+    for (i = 0; i < array.length; i++){
+        if (array[i] === filter)
+            resArray.push(array[i]);
+    }
+    return resArray;
+}
+
+function sumNumbers(array){
+    let result = 0;
+    for (i = 0; i < array.length; i++){
+        result += array[i];
+    }
+    return result;
+}
+
+function squareNumbersAndAddThree(array){
+    let resultArray = new Array;
+    for (i = 0; i < array.length; i++){
+        resultArray[i] = ((array[i]*array[i])+3);
+    }
+    return resultArray;
+}
+
+function logAllNumbers(array){
+    for (i = 0; i < array.length; i++){
+        console.log(array[i]);
+    }
+}
+
+function handleForm (form) {
+    let fieldValue = form.inputbox.value;
+    document.getElementById('mytext').value = fieldValue + "!";
+}
+
+console.log(joinArrays(people1, people2));
+console.log(getLast(people2));
+console.log(filterArray(people1, "John Doe"));
+console.log(sumNumbers(numbers));
+console.log(squareNumbersAndAddThree(numbers));
+logAllNumbers;
