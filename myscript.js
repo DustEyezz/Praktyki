f5aef9f479d3a4358784385abec9f3816d0e7afc
DustEyezz <75487794+DustEyezz@users.noreply.github.com>
2022-11-17 19:10:45 +0100

    cleaner

--- myscript.js
+++ myscript.js
@@ -7,18 +7,17 @@ const books = [
     {title: 'World Ender', pages: 199, genre: 'fantasy'},
     {title: 'Paranormal', pages: 200, genre: 'thriller'},
 ];
+//tworzymy funkcje
 const filterTitleStartsWithTotal = (list) => list.filter((book) => book.title.startsWith('Total'));
 const filterGenreIsFantasy = (list) => list.filter((book) => book.genre === 'fantasy');
-
 const mapToPages = (list) => list.map(({pages}) => pages);
 const sumPages = (book) => book.reduce((currSum, newPage) => currSum + newPage)
-
+//tworzymy kompozycje
 const badCompose = (fn1, fn2, fn3) => (x) => fn3(fn2(fn1(x)));
-
 const compose = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x);
-
+//tworzymy funkcję używającą kompozycji
 const titleStartsWithTotalPages = compose(sumPages, mapToPages, filterTitleStartsWithTotal);
 const genreIsFantasyPages = compose(sumPages, mapToPages, filterGenreIsFantasy);
-
+//wypisujemy dla książek
 console.log(titleStartsWithTotalPages(books));
 console.log(genreIsFantasyPages(books));
